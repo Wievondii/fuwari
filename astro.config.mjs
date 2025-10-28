b4c12a42371e1ad0c2f5ee36c3820bc7084dc7c0
Wievondii <2658782385@qq.com>
2025-10-28 12:28:27 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://265878.xyz",
+	site: "https://fuwari-bz1.pages.dev",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
